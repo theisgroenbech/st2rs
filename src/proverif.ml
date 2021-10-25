b6ddef72820557aadde53207f9273f22e6ab758d
Anders Sparrevohn <anders-sparrevohn@hotmail.com>
2021-10-25 11:55:17 +0200

    Add data formats and some process printing
    Co-authored-by: Elitsa Marinovska <em-mar@outlook.dk>

--- src/proverif.ml
+++ src/proverif.ml
@@ -1,5 +1,5 @@
 open Types
-
+open Localtypes
 
 let rec show_params = function
   [] -> ""
@@ -29,15 +29,30 @@ and show_equation equation names_and_types =
   match equation with
   | (t1, t2) -> "equation forall " ^ (show_equation_params equation names_and_types) ^ ";\n\t" ^ (show_term t1) ^ " = " ^ (show_term t2)
 
+and show_local_type = function
+    LSend(p, t, local_type) -> "out(c, " ^ show_term t  ^");\n"^ show_local_type local_type
+  | LNew (ident, data_type, local_type) -> "new " ^ ident ^ " : " ^ show_dtype data_type ^ ";\n" ^ show_local_type local_type
+  | LLet (ident, term, local_type) -> "let " ^ show_pattern ident ^ " = " ^ show_term term ^ " in\n" ^ show_local_type local_type
+  | LRecv (principal, pattern, term, local_type) -> "in(c, " ^ show_pattern pattern ^ ": bitstring);\n" ^ show_local_type local_type
+  | LEvent (ident, termlist, local_type) -> "event " ^ ident ^ "(" ^ show_term_list termlist ^ ");\n" ^ show_local_type local_type
+  | LLocalEnd -> "0."
+
+and show_format = function
+  (name, types) -> "fun " ^ name ^ "(" ^ (String.concat ", " (List.map (fun t -> show_dtype t) types)) ^ "): bitstring [data]."
+
 let proverif (pr:problem): unit =
   Printf.printf  "(* Protocol: %s *)\n\n" pr.name;
   let function_types = List.map (fun f -> build_function f) pr.functions in
   List.iter (fun t -> 
     Printf.printf "type %s.\n" (show_dtype t)) pr.types;
   Printf.printf "%s\n" "";
+  List.iter (fun f -> 
+    Printf.printf "%s\n" (show_format f)) pr.formats;
+  Printf.printf "%s\n" "";
   List.iter (fun t -> 
     Printf.printf "%s.\n" (show_function t)) pr.functions;
-    Printf.printf "%s\n" "";
+  Printf.printf "%s\n" "";
   List.iter (fun e -> 
     Printf.printf "%s.\n" (show_equation e function_types)) pr.equations;
+  List.iter (fun (p, b) -> Printf.printf "\n%s\n" (show_local_type (to_local_type pr.protocol p))) pr.principals;
   Printf.printf "%s" "process 0" (* Just for testing until the rest is done *)
